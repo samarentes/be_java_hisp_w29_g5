48603e98a46c1838c36eec1ee8da5f412d4aa75f
Sergio Marentes <sergio.marentes@mercadolibre.com.co>
2025-01-21 12:12:50 -0500

    adding utilitarie class for exceptiosn messages

---
+++ src/main/java/com/social_media/social_media/utils/MessagesExceptions.java
@@ -0,0 +1,10 @@
+package com.social_media.social_media.utils;
+
+public final class MessagesExceptions {
+    private MessagesExceptions() {
+    }
+
+    public static final String SELLER_ID_NOT_EXIST = "El vendedor no existe";
+    public static final String BUYER_ID_NOT_EXIST = "El comprador no existe";
+    public static final String FOLLOWED_USER_NOT_SELLER = "El usuario a seguir no es un vendedor";
+}
