3ee11eb73c1648711e47612b3d93fd1cd9bdf6de
Sergio Marentes <sergio.marentes@mercadolibre.com.co>
2025-01-21 12:19:34 -0500

    adding exceptions controller

---
+++ src/main/java/com/social_media/social_media/dto/ExceptionDto.java
@@ -0,0 +1,12 @@
+package com.social_media.social_media.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class ExceptionDto {
+    private String message;
+}
---
+++ src/main/java/com/social_media/social_media/exception/NotFoundException.java
@@ -0,0 +1,7 @@
+package com.social_media.social_media.exception;
+
+public class NotFoundException extends RuntimeException {
+    public NotFoundException(String message) {
+        super(message);
+    }
+}
