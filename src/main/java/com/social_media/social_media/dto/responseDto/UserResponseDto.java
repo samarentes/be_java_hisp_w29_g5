12b3e983f5bd0c189a9fc56b7a8e652a361133ff
Sergio Marentes <sergio.marentes@mercadolibre.com.co>
2025-01-21 14:48:18 -0500

    fixing userDto to response

--- src/main/java/com/social_media/social_media/dto/responseDto/UserResponseDto.java
+++ src/main/java/com/social_media/social_media/dto/responseDto/UserResponseDto.java
@@ -1,7 +1,5 @@
 package com.social_media.social_media.dto.responseDto;
 
-import java.util.List;
-
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -9,8 +7,7 @@ import lombok.NoArgsConstructor;
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor
-public class FollowersResponseDto {
+public class UserResponseDto {
     private Long user_id;
     private String user_name;
-    private List<UserResponseDto> followers;
 }
