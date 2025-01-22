91e9c8ff700743744c93095238086483bc39dad1
sebaschb <s.chacon11@uniandes.edu.co>
2025-01-22 15:43:14 -0500

    US_0005
    * add: logic creted post
    
    * add: logic for created post
    
    * fix: changes for PostId in responseDto
    
    ---------
    
    Co-authored-by: Sebastian Chacon Benitez <sechacon@co000mtgqcqyx7w.ml.com>

--- src/main/java/com/social_media/social_media/controller/UserController.java
+++ src/main/java/com/social_media/social_media/controller/UserController.java
@@ -1,10 +1,9 @@
 package com.social_media.social_media.controller;
 
-import com.social_media.social_media.service.IUserService;
+import com.social_media.social_media.service.user.IUserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @RequiredArgsConstructor
 @RestController
--- src/main/java/com/social_media/social_media/service/user/UserServiceImpl.java
+++ src/main/java/com/social_media/social_media/service/user/UserServiceImpl.java
@@ -1,5 +1,6 @@
-package com.social_media.social_media.service;
+package com.social_media.social_media.service.user;
 
+import com.social_media.social_media.entity.User;
 import com.social_media.social_media.repository.follow.IFollowRepository;
 import com.social_media.social_media.repository.post.IPostRepository;
 
@@ -8,11 +9,15 @@ import org.springframework.stereotype.Service;
 
 import com.social_media.social_media.repository.user.IUserRepository;
 
+import java.util.Optional;
+
 @RequiredArgsConstructor
 @Service
 public class UserServiceImpl implements IUserService {
     private final IUserRepository userRepository;
     private final IPostRepository postRepository;
     private final IFollowRepository followRepository;
+
+
 
 }
