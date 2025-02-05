954b0a477fda2d3aa27b5d845ba4354ed4398e07
Belén Brinso <bbrinso@frba.utn.edu.ar>
2025-02-05 12:57:45 -0300

    create test packages structure

---
+++ src/test/java/com/social_media/social_media/integrationTest/I0010.java
@@ -0,0 +1,4 @@
+package com.social_media.social_media.integrationTest;
+
+public class I0010 {
+}
---
+++ src/test/java/com/social_media/social_media/unitTest/serviceTest/T0013.java
@@ -0,0 +1,4 @@
+package com.social_media.social_media.unitTest.serviceTest;
+
+public class T0013 {
+}
