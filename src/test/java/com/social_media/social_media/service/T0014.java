2f9a5e792c2b82c6a15b1df75be93cbf36ad69b9
Sergio Marentes <sergio.marentes@mercadolibre.com.co>
2025-02-06 10:20:02 -0500

    adding find by id on post with stock

--- src/test/java/com/social_media/social_media/service/T0014.java
+++ src/test/java/com/social_media/social_media/service/T0014.java
@@ -15,13 +15,19 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import com.social_media.social_media.TestUtils;
+import com.social_media.social_media.dto.request.ProductRequestDto;
 import com.social_media.social_media.dto.request.StockRequestDto;
+import com.social_media.social_media.dto.response.PostDetailResponseDto;
+import com.social_media.social_media.dto.response.ProductResponseDto;
 import com.social_media.social_media.dto.response.StockResponseDto;
 import com.social_media.social_media.entity.Post;
+import com.social_media.social_media.entity.Product;
+import com.social_media.social_media.entity.Stock;
 import com.social_media.social_media.entity.User;
 import com.social_media.social_media.exception.NotFoundException;
 import com.social_media.social_media.repository.post.PostRepositoryImpl;
 import com.social_media.social_media.repository.stock.StockRepository;
+import com.social_media.social_media.service.post.PostServiceImpl;
 import com.social_media.social_media.service.stock.StockServiceImpl;
 import com.social_media.social_media.utils.MessagesExceptions;
 
@@ -35,6 +41,9 @@ public class T0014 {
 
     @InjectMocks
     StockServiceImpl stockService;
+
+    @InjectMocks
+    PostServiceImpl postService;
 
     @Test
     @DisplayName("T-0014 Create Stock Test 200")
@@ -69,6 +78,56 @@ public class T0014 {
         NotFoundException thrown = assertThrows(
                 NotFoundException.class,
                 () -> stockService.createStock(post.getPostId(), requestBodyDto));
+
+        String expectedMessage = MessagesExceptions.POST_NOT_FOUND;
+        String actualMessage = thrown.getMessage();
+
+        assertEquals(expectedMessage, actualMessage);
+    }
+
+    @Test
+    @DisplayName("T-0014 Find post with stock Test 200")
+    void findPostWithStockTest200() {
+
+        User userSeller = TestUtils.createRandomUser();
+
+        Post post = TestUtils.createRandomPost(userSeller.getUserId());
+
+        Stock stock = new Stock(post.getPostId(), 15L);
+
+        StockResponseDto stockResponseDto = new StockResponseDto(15L);
+
+        Product product = post.getProduct();
+        ProductResponseDto productResponseDto = new ProductResponseDto(product.getProductId(), product.getProductName(),
+                product.getType(), product.getBrand(), product.getColor(), product.getNotes());
+
+        when(postRepository.findById(post.getPostId())).thenReturn(Optional.of(post));
+
+        when(stockRepository.findByPostId(post.getPostId())).thenReturn(Optional.of(stock));
+
+        PostDetailResponseDto expectedResponse = new PostDetailResponseDto(post.getPostId(), userSeller.getUserId(),
+                stockResponseDto, post.getDate(), productResponseDto, post.getCategory(), post.getPrice(), true,
+                post.getDiscount());
+        PostDetailResponseDto actualResponse = postService.searchById(post.getPostId());
+
+        assertThat(actualResponse)
+                .usingRecursiveComparison()
+                .isEqualTo(expectedResponse);
+    }
+
+    @Test
+    @DisplayName("T-0014 Find Post with stock not found exception")
+    void findPostWithStockFailPostNotFound() {
+
+        User userSeller = TestUtils.createRandomUser();
+
+        Post post = TestUtils.createRandomPost(userSeller.getUserId());
+
+        when(postRepository.findById(post.getPostId())).thenReturn(Optional.empty());
+
+        NotFoundException thrown = assertThrows(
+                NotFoundException.class,
+                () -> postService.searchById(post.getPostId()));
 
         String expectedMessage = MessagesExceptions.POST_NOT_FOUND;
         String actualMessage = thrown.getMessage();
