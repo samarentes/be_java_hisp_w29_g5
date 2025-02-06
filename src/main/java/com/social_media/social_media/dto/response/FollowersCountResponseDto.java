316bcc4d00ec76a57ff5c61c34d2e0268ee3abdb
sebaschb <sebaschb>
2025-02-06 09:13:49 -0500

    fix: change all imports for loombok

--- src/main/java/com/social_media/social_media/dto/response/FollowersCountResponseDto.java
+++ src/main/java/com/social_media/social_media/dto/response/FollowersCountResponseDto.java
@@ -1,6 +1,10 @@
 package com.social_media.social_media.dto.response;
 
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.EqualsAndHashCode;
 
 @Getter
 @AllArgsConstructor
