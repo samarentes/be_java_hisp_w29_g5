36ef807f6e8f0a4a441be4a11717c6ee53735604
rlopezmendez_meli <roberto.lopezmendez@mercadolibre.com.co>
2025-02-07 11:38:41 -0500

    fix importaos on FollowedResponseDTo

--- src/main/java/com/social_media/social_media/dto/response/FollowedResponseDto.java
+++ src/main/java/com/social_media/social_media/dto/response/FollowedResponseDto.java
@@ -1,8 +1,13 @@
 package com.social_media.social_media.dto.response;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.EqualsAndHashCode;
+
 import java.util.List;
 
-import lombok.*;
 
 @Builder
 @Getter
